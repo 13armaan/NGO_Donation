c662d6dd879ec8dd91ef4b4bab1b7c85710230d7
Armaan Sharma <sharmaarmaan.9871@gmail.com>
2026-01-10 04:41:13 +0530

    integrated the database made the tables for users and donation and made a prototype register page and connected it to fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from supabase_client import supabase
+from fastapi.middleware.cors import CORSMiddleware
 
-app=FastAPI();
+app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
     return {"status":"NGO backend running"}
+
+class UserCreate(BaseModel):
+    email:str
+    password:str
+    role:str
+
+@app.post("/register")
+def register(user:UserCreate):
+    supabase.table("users").insert({
+        "email":user.email,
+        "password":user.password,
+        "role":user.role
+    }).execute()
+    return{"message":"registered"}
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,51 @@
+import {useState} from "react";
+
 function Register(){
-    return <h2>Register</h2>;
+    const[email, setEmail]=useState("");
+    const[password, setPassword]=useState("");
+    const[role, setRole]=useState("user");
+
+    async function handleRegister(){
+    const res=await fetch("http://127.0.0.1:8000/register",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            email:email,
+            password:password,
+            role: role
+        })
+    });
+    const data =await res.json();
+    alert(data.message);
+    }
+    return(
+        <div>
+            <h2>Register</h2>
+    
+            <input
+            placeholder="email"
+            onChange={(e)=>setEmail(e.target.value)}
+            />
+            <br />
+            <input
+            placeholder="password"
+            onChange={(e)=>setPassword(e.target.value)}
+            />
+            <br />
+    
+            <select onChange={(e)=>setRole(e.target.value)}>
+                <option value={"admin"}>Admin</option>
+                <option value={"user"}>User</option>
+            </select>
+    
+        <br />
+    
+        <button onClick={handleRegister}>Register</button>
+        </div>
+    )
 }
+
 
 export default Register;
